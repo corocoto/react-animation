1a9f7f28330493bf2600e7c0e1a50ea6182f4099
Artem Gusev <gusev2014russia@mail.ru>
2020-08-02 21:51:48 +0300

    adds animation for List's actions (add or remove item)

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import './List.css';
 
 class List extends Component {
@@ -25,19 +25,28 @@ class List extends Component {
 
     render() {
         const listItems = this.state.items.map((item, index) => (
-            <li
+            <CSSTransition
                 key={index}
-                className="ListItem"
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
+                classNames="fade"
+                timeout={300}
+            >
+                <li
+                    className="ListItem"
+                    onClick={() => this.removeItemHandler(index)}
+                >{item}</li>
+            </CSSTransition>
         ));
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+                <TransitionGroup
+                    component="ul"
+                    className="List"
+                >
                     {listItems}
-                </ul>
+                </TransitionGroup>
             </div>
         );
     }
